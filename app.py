1656ea425d749937adbfbd897f75f2caa08b91d9
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-06-29 18:40:00 +0545

    added error checks for wrong username

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ import json
 
 
 
-def get_activity():
+def main():
     git_username = input("Enter your github user_name: ")
     res = requests.get(f"https://api.github.com/users/{git_username}/events")
 
     data = res.json()
+
+    if len(data) == 0:
+        print(f"{git_username} not found.")
+    git_activity(git_username, data)  
+
+
+def git_activity(git_username,data):
     for items in data:
         if items["type"] == "CreateEvent":
             print(f"{git_username} created a new repository. {items["repo"]["name"]}")
@@ -15,8 +22,9 @@ def get_activity():
             print(f"{git_username} pushed a commit to {items["repo"]["name"]}")
         elif items["type"] == "PullRequestEvent":
             print(f"{git_username} created a new pull request on {items["repo"]["name"]}")
-        
+        elif items["type"] == "WatchEvent" and items["payload"]["action"] == "started":
+            print(f"{git_username} Starred {items["repo"]["name"]}")
 
 
 if __name__ == "__main__":
-    get_activity()
+    main()
